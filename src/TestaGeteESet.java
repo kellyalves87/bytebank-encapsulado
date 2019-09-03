b99ab3cc14377496fb3026860a9911254de3505a
Kelly <kellyp.alves87@gmail.com>
2019-09-02 23:56:02 -0300

    first commit

---
+++ src/TestaGeteESet.java
@@ -0,0 +1,27 @@
+
+public class TestaGeteESet {
+
+	public static void main(String[] args) {
+		Conta conta = new Conta(1337, 24226);
+		
+		System.out.println(conta.getNumero());
+		
+		Cliente paulo = new Cliente();
+		paulo.setNome("Paulo Silveira");
+		
+		conta.setTitular(paulo);
+		
+		System.out.println(conta.getTitular().getNome());
+		
+		conta.getTitular().setProfissao("Programador");
+		// agora em duas linhas:
+		Cliente titularDaConta = conta.getTitular();
+		titularDaConta.setProfissao("Programador");
+		
+		System.out.println(titularDaConta);
+		System.out.println(paulo);
+		System.out.println(conta.getTitular());
+		
+				
+	}
+}
